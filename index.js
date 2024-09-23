e239cfd884c4b8d4c185497be8baadcde0528b3c
Mary Monchari Gichana <marygichana95@gmail.com>
2024-09-23 22:57:55 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  
+    if(distance <= 400){
+    return('This one is on me!');
+    }
+    else if(distance > 400 && distance < 2000){
+      return('That will be twenty bucks.');
+    }
+    else if(distance >2000 && distance <= 2500){
+      return('I will gladly take your thirty bucks.');
+    }
+    else{
+      return('No can do.');
+    }
+    
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+return  city === 'NYC'?'Ok, sounds good.':'No go.';
+
 }
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case('generous'):
+    return  'Thank you so much.';
+       break;
+    case('not as generous'):
+    return  'Thank you.';
+       break;
+    default:
+    return 'Bye.';
+  }
+              
+
+}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
